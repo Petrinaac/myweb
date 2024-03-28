f7b772bed18063baa3401c184f9142070055d6c3
Petrinaac <122981065+Petrinaac@users.noreply.github.com>
2024-03-28 12:58:48 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", reveal)
+
+function reveal() {
+  let reveals = document.querySelectorAll('.reveal')
+
+  for (let i = 0; i < reveals.length; i++) {
+  let windowheight = window.innerHeight;
+  let revealtop = reveals[i].getBoundingClientRect().top
+  let revealpoint = 150;
+
+  if(revealtop < windowheight - revealpoint) {
+  reveals[i].classList.add('active');
+  }
+  }
+}
